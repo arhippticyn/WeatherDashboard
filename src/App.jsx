de509b3d71e838204935fb340ad5badb0c04ed32
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2025-08-17 23:48:57 +0300

    add shablon btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import "./App.css";
+import Button from "./components/Button";
 
 function App() {
-  return <></>;
+  return <>
+  <Button text={'hello'}  style={{ width: "150px", height: "50px" }}  />
+  </>;
 }
 
 export default App;
