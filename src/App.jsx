21c14166fa393789ed0931d49c4e3b17e791c92b
Egor2090 <egorvlasov504@gmail.com>
2025-08-23 20:02:31 +0300

    Creating a section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header/Header";
 import Main from "./components/Main";
+import Pets from "./components/Pets/Pets"
 
 function App() {
   return (
     <div className="page-wrapper">
       <Header />
+      <Pets />
       <Main />
+      
     </div>
   )
 }
