6b60ffe202a401868c6652b43a6fb9d34d4b0281
kushik <lada.chokol@gmail.com>
2025-08-23 20:40:23 +0300

    -

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Nature from "./Nature/Nature";
+import WeatherForecast from "./WeatherForecast";
 
 const Main = () => {
   return (
     <div className="main-wrapper">
       <main className="main">
         <Nature />
+        <WeatherForecast />
       </main>
     </div>
   )
