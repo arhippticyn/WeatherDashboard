54235732cce9633537962a392c1168293abf16c8
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2025-08-24 19:35:14 +0300

    remake

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Nature from "./Nature/Nature";
+import WeatherForecast from "./WeatherForecast";
+import Pets from "./Pets/Pets";
 
 const Main = () => {
   return (
     <div className="main-wrapper">
       <main className="main">
+        <Pets />
         <Nature />
+        <WeatherForecast />
       </main>
     </div>
   )
