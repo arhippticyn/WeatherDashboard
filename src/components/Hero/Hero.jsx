546d86157867bc99a06b993db07837fda3349e09
Big_bob <svaitoslavtt@gmail.com>
2025-08-24 19:34:17 +0300

    add hero bg

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import "../../styles/styles.scss"; // головний файл стилів
-import bgImage from "../../assets/hero-bg.jpg";
+import "../../components/_hero.scss"; 
 
 const Hero = () => {
   return (
-    <section className="hero" style={{ backgroundImage: `url(${bgImage})` }}>
+    <section className="hero">
       <div className="overlay">
         <div className="hero-content">
-          <h1 className="hero-title">Weather dashboard</h1>
-          <p className="hero-subtitle">
+          <h1 className="hero-content-title">Weather dashboard</h1>
+          <p className="hero-content-subtitle">
             Create your personal list of favorite cities and always be aware of the weather.
           </p>
-          <p className="hero-date">October 2023 <br /> Friday, 13th</p>
+          <p className="hero-content-date">
+            October 2023 <br /> Friday, 13th
+          </p>
 
           <div className="search-box">
             <input type="text" placeholder="Search location..." />
