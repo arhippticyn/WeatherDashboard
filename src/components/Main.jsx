d71473c39385ef8b234353f95164c6f73e00fd30
Vladyslav Sheludko <vladsh2103@gmail.com>
2025-08-26 11:52:53 -0400

    graph done, done some upgrades to the project too

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Nature from "./Nature/Nature";
+import Graph from "./Graph/Graph";
 
 const Main = () => {
   return (
     <div className="main-wrapper">
       <main className="main">
         <Nature />
+        <Graph
+          data={[
+            10, 20, 30, 20, 10, 20, 40, 50, 20, 30, 10, 30, 20, 10, 20, 40, 50,
+            20, 30, 10,
+          ]}
+        ></Graph>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
