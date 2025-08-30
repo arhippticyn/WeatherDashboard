dd9f466f7557f9cb8d7caff64091901166dfe874
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2025-08-30 19:43:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main";
 import News from "./components/News/News"
 import Footer from "./components/Footer";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="page-wrapper">
       <Header />
-
-
+      <Hero />
       <Main />
     <Footer />
     </div>
