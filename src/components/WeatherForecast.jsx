17c61ef0584cfc3d54f30612e2d032de1c907412
kushik <lada.chokol@gmail.com>
2025-09-06 20:11:06 +0300

    new_8days

--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -23,11 +23,13 @@ const WeatherForecast = () => {
       const response = await axios.get(
         `/forecast?q=${selectedCity}&units=metric&appid=${WEATHER_API_KEY}`
       );
-<<<<<<< Updated upstream
-=======
+
 
       setForecast(response.data.list);
->>>>>>> Stashed changes
+
+
+      setForecast(response.data.list);
+
 
       setForecast(response.data.list);
     } catch (error) {
