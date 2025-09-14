f67c511c41cfd62b5ade798fddfe438000876fd2
Vladyslav Sheludko <128414119+sheludkovladyslav@users.noreply.github.com>
2025-09-14 00:21:21 -0400

    fix styles hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,30 +3,46 @@ import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 
 const Hero = ({ query, setQuery, onSearch }) => {
-  const [date, setDate] = useState(new Date())
+  const [date, setDate] = useState(new Date());
 
-  useEffect(() =>{
-    const now = new Date()
-    const msUntilMidnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1) - now
+  useEffect(() => {
+    const now = new Date();
+    const msUntilMidnight =
+      new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1) - now;
 
-    const timer = setTimeout(() => setDate(new Date()), msUntilMidnight)
-    return () => clearTimeout(timer)
-  }, [date])
+    const timer = setTimeout(() => setDate(new Date()), msUntilMidnight);
+    return () => clearTimeout(timer);
+  }, [date]);
 
   const months = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ];
 
   const days = [
-    "Sunday", "Monday", "Tuesday", "Wednesday",
-    "Thursday", "Friday", "Saturday"
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
   ];
 
-  const month = months[date.getMonth()]
-  const year = date.getFullYear()
-  const dayName = days[date.getDay()]
-  const day = date.getDate()
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+  const dayName = days[date.getDay()];
+  const day = date.getDate();
 
   const getDaySuffix = (n) => {
     if (n % 10 === 1 && n % 100 !== 11) return "st";
@@ -40,15 +56,19 @@ const Hero = ({ query, setQuery, onSearch }) => {
       <div className="overlay">
         <div className="hero-content">
           <h1 className="hero-content-title">Weather dashboard</h1>
-          <div className="hero-content-down">
-            <p className="hero-content-subtitle">
-            Create your personal list of favorite cities and always be aware of
-            the weather.
-          </p>
-          <hr />
-          <p className="hero-content-date">
-            {month} {year} <br /> {dayName}, {day}{getDaySuffix(day)}
-          </p>
+
+          <div className="hero__date">
+            <div className="hero-content-down">
+              <p className="hero-content-subtitle">
+                Create your personal list of favorite cities and always be aware
+                of the weather.
+              </p>
+              <hr />
+              <p className="hero-content-date">
+                {month} {year} <br /> {dayName}, {day}
+                {getDaySuffix(day)}
+              </p>
+            </div>
           </div>
 
           <div className="search-box">
